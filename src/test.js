f4fcd0597d1c91299a57197911749ebab2a8b60c
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-05-15 15:29:01 -0400

    Add tests for Ship class (using carrier instance)
    Test whether carrier is hit at a given coord, is hit but not sunk, and is fully sunk.

--- src/test.js
+++ src/test.js
@@ -1,5 +1,23 @@
-import { sum } from "./index.js";
+import { carrier } from "./ship.js";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("Carrier is hit at coord 5", () => {
+  expect(carrier.hit(4)).toBe(true);
+});
+
+test("Carrier is hit but not sunk until after 5 hits", () => {
+  carrier.hit(0);
+  carrier.hit(1);
+  carrier.hit(2);
+  carrier.hit(3);
+  // carrier.hit(4);
+  expect(carrier.isSunk()).toBe(false);
+});
+
+test("Carrier sunk after 5 hits", () => {
+  carrier.hit(0);
+  carrier.hit(1);
+  carrier.hit(2);
+  carrier.hit(3);
+  carrier.hit(4);
+  expect(carrier.isSunk()).toBe(true);
 });
