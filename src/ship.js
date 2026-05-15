b9bbbe220fa1452a4e522eb505e1a2444490c594
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-05-15 15:23:06 -0400

    Create Ship and Node classes
    Note the types of ships and their lengths, add Ship properties for length, area, hits and sunk. Create methods buildShip(), hit() and isSunk().
    Also create coord and isHit properties on Node class for when ship is placed on the gameboard.
    Create carrier instance and export for testing.

--- src/ship.js
+++ src/ship.js
@@ -1,1 +1,47 @@
-export const wpTest = "Test";
+// Ship types and lengths:
+
+// Carrier: 5
+// Battleship: 4
+// Cruiser: 3
+// Submarine: 3
+// Destroyer: 2
+
+class Ship {
+  constructor(len) {
+    this.len = len;
+    this.area = this.buildShip(len);
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  buildShip(len) {
+    const area = [];
+    for (let i = 0; i < len; i++) {
+      area.push(new Node());
+    }
+    return area;
+  }
+
+  hit(coord) {
+    this.hits++;
+    return (this.area[coord].isHit = true);
+  }
+
+  isSunk() {
+    if (this.hits > this.len) {
+      this.sunk = true;
+    }
+    return this.sunk;
+  }
+}
+
+class Node {
+  constructor(coord) {
+    this.coord = coord;
+    this.isHit = false;
+  }
+}
+
+const carrier = new Ship(5);
+
+export { carrier };
