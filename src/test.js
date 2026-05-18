a4364706aa7d474664415972922f63a970971917
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-05-18 13:48:35 -0400

    Test vertical/horizontal positioning of ships on gameboard

--- src/test.js
+++ src/test.js
@@ -1,4 +1,6 @@
 import { carrier } from "./ship.js";
+import { battleship } from "./ship.js";
+import { board } from "./gameboard.js";
 
 test("Carrier is hit at coord 5", () => {
   expect(carrier.hit(4)).toBe(true);
@@ -20,4 +22,17 @@ test("Carrier sunk after 5 hits", () => {
   carrier.hit(3);
   carrier.hit(4);
   expect(carrier.isSunk()).toBe(true);
+});
+
+test("Carrier placed at [0, 0] with horizontal direction should show the following coords: [0, 0], [0, 1], [0, 2], [0, 3], [0, 4]", () => {
+  expect(carrier.area[0].coords).toMatchObject([0, 0]);
+  expect(carrier.area[1].coords).toMatchObject([0, 1]);
+  expect(carrier.area[2].coords).toMatchObject([0, 2]);
+  expect(carrier.area[3].coords).toMatchObject([0, 3]);
+  expect(carrier.area[4].coords).toMatchObject([0, 4]);
+});
+
+test("Battleship placed at [0, 0] with vertical direction should show the following coords: [0, 0], [1, 0], [2, 0], [3, 0]", () => {
+  expect(battleship.area[0].coords).toMatchObject([0, 0]);
+  expect(battleship.area[1].coords).toMatchObject([1, 0]);
 });
