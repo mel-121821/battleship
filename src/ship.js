7cded210076c9a2bbe5846dee1ceb7d9eb69f249
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-05-18 13:51:14 -0400

    Update Ship class and subsequent methods to take coords and direction as args.
    Also update ship names to reflect a more recent version of the game and create an instance of a battleship for testing

--- src/ship.js
+++ src/ship.js
@@ -2,29 +2,37 @@
 
 // Carrier: 5
 // Battleship: 4
-// Cruiser: 3
+// Destroyerr: 3
 // Submarine: 3
-// Destroyer: 2
+// Patrol Boat: 2
 
 class Ship {
-  constructor(len) {
+  constructor(coords, dir, len) {
+    this.coords = coords;
+    this.dir = dir;
     this.len = len;
-    this.area = this.buildShip(len);
+    this.area = this.buildShip(coords, dir, len);
     this.hits = 0;
     this.sunk = false;
   }
 
-  buildShip(len) {
+  buildShip(coords, dir, len) {
     const area = [];
-    for (let i = 0; i < len; i++) {
-      area.push(new Node());
+    if (dir === "hor") {
+      for (let i = 0; i < len; i++) {
+        area.push(new Node(coords[0], coords[1]++));
+      }
+    } else {
+      for (let i = 0; i < len; i++) {
+        area.push(new Node(coords[0]++, coords[1]));
+      }
     }
     return area;
   }
 
-  hit(coord) {
+  hit(coords) {
     this.hits++;
-    return (this.area[coord].isHit = true);
+    return (this.area[coords].isHit = true);
   }
 
   isSunk() {
@@ -36,12 +44,14 @@ class Ship {
 }
 
 class Node {
-  constructor(coord) {
-    this.coord = coord;
+  constructor(x, y) {
+    this.coords = [x, y];
     this.isHit = false;
   }
 }
 
-const carrier = new Ship(5);
+const carrier = new Ship([0, 0], "hor", 5);
 
-export { carrier };
+const battleship = new Ship([0, 0], "vert", 4);
+
+export { carrier, battleship };
