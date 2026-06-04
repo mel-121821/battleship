e3e85c7df3d78db1d98dc82ceec8fc9817afcd8a
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-06-04 10:49:54 -0400

    Remove generateShipCoords and add reportSunk tests
    - Add a placeShip call for destroyer to be used later in recieveAttack
    test
    - Simulate 2 sunk boats and create tests for reportSunk

--- src/test.js
+++ src/test.js
@@ -1,14 +1,6 @@
 import { board } from "./gameboard.js";
 
-test("generateShipCoords with the args: ([0, 0], 'hor', 4) should return the following coords: [0, 0], [0, 1], [0, 2], [0, 3] ", () => {
-  expect(board.generateShipCoords(0, 0, "x-axis", 4)).toMatchObject([
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-  ]);
-});
-
+// Placement tests
 test("Carrier placed at [1, 1] with horizontal direction should show the following coords as occupied: [1, 1], [1, 2], [1, 3], [1, 4], [1, 5]", () => {
   expect(
     board.placeShip(1, 1, "x-axis", board.ships[0]).shipCoords
@@ -30,6 +22,40 @@ test("Battleship placed at [0, 0] with vertical direction should show the follow
     [2, 0],
     [3, 0],
   ]);
+});
+
+test("Destroyer placed at [4, 4] with horizontal direction should show the following coords: [4, 4], [4, 5], [4, 6]", () => {
+  expect(
+    board.placeShip(4, 4, "x-axis", board.ships[2]).shipCoords
+  ).toMatchObject([
+    [4, 4],
+    [4, 5],
+    [4, 6],
+  ]);
+});
+
+// Hit/sunk tests
+test("Carrier is hit 5 times and sunk.", () => {
+  expect(board.receiveAttack(1, 1)).toBeTruthy();
+  expect(board.receiveAttack(1, 2)).toBeTruthy();
+  expect(board.receiveAttack(1, 3)).toBeTruthy();
+  expect(board.receiveAttack(1, 4)).toBeTruthy();
+  expect(board.receiveAttack(1, 5)).toBeTruthy();
+});
+
+test("reportSunk logs the num of ships sunk plus the num of times it is called in the tests (this is a side effect as we are calling reportSunk an extra time outside of its normal flow", () => {
+  expect(board.reportSunk()).toBe(2);
+});
+
+test("Battleship is hit 4 times and sunk.", () => {
+  expect(board.receiveAttack(0, 0)).toBeTruthy();
+  expect(board.receiveAttack(1, 0)).toBeTruthy();
+  expect(board.receiveAttack(2, 0)).toBeTruthy();
+  expect(board.receiveAttack(3, 0)).toBeTruthy();
+});
+
+test("reportSunk logs the num of ships sunk plus the num of times it is called in the tests (this is a side effect as we are calling reportSunk an extra time outside of its normal flow", () => {
+  expect(board.reportSunk()).toBe(4);
 });
 
 test("Patrol boat placed at [0, 0] with horizontal direction should return undefined as a boat cannot be placed there. That square is occupied by the battleship", () => {
@@ -65,5 +91,5 @@ test("receiveAttack should show affected square (no boat) as received attack", (
 });
 
 test("receiveAttack should show affected square (boat exists) as received attack", () => {
-  expect(board.receiveAttack(0, 0)).toBe(true);
+  expect(board.receiveAttack(4, 4)).toBe(true);
 });
