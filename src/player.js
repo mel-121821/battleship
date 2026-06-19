1952ca537ff20abbe6dee3ddb99f24b3cec73a13
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-06-19 15:17:55 -0400

    refactor: remove redundant Computer class

--- src/player.js
+++ src/player.js
@@ -1,15 +1,10 @@
-import { Gameboard } from "./gameboard";
+import { Gameboard } from "./gameboard.js";
 
 class Player {
   constructor(name) {
     this.name = name;
-    this.board = new Gameboard();
+    this.data = new Gameboard();
   }
 }
 
-class Computer {
-  constructor() {
-    this.name = "Computer";
-    this.board = new Gameboard();
-  }
-}
+export { Player };
