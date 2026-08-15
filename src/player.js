470fa2c9390d8f249edba5c4a0d5ce230ed31eaf
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-08-15 14:35:01 -0400

    FEAT: create Computer class and methods
    - extend Computer class from player and add opponent property
    - create method setOpponent() to add reference to opposing player
    - create attackOpponent() method to randomly select coords and attack
    opponent's board
    - name proximityAttack() method

--- src/player.js
+++ src/player.js
@@ -1,11 +1,48 @@
 import { Gameboard } from "./gameboard.js";
 
 class Player {
-  constructor(name, pCode) {
+  type = "player";
+  constructor(pCode, name) {
     this.name = name;
     this.pCode = pCode;
-    this.data = new Gameboard(name, pCode);
+    this.data = new Gameboard(this);
   }
 }
 
-export { Player };
+class Computer extends Player {
+  type = "computer";
+  opponent = null;
+
+  constructor(pCode) {
+    super(pCode);
+    this.name = "Computer";
+
+    this.attackOpponent_bound = this.attackOpponent.bind(this);
+  }
+
+  setOpponent(board) {
+    console.log(board);
+    this.opponent = board;
+    console.log(this.opponent);
+  }
+
+  generateAttackCoords() {
+    return [x, y];
+  }
+
+  attackOpponent() {
+    // const coords = this.generateAttackCoords();
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    console.log(this.opponent);
+    this.opponent.receiveAttack(x, y);
+  }
+
+  // attackOpponent_Delayed() {
+  //   setTimeout(this.attackOpponent, 1000);
+  // }
+
+  proximityAttack() {}
+}
+
+export { Player, Computer };
